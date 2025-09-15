1faae4cf99bcc1cd483c2926cf54ab6e08baead0
Sandeep Thalapanane <ssandy086@gmail.com>
2025-09-15 13:54:45 -0700

    Update lambda1 again

--- src/lambda1/index.js
+++ src/lambda1/index.js
@@ -1,7 +1,7 @@
 // lambda1/index.js
 export async function lambdaHandler(event, context) {
 return {
-  statusCode: 300,
+  statusCode: 400,
   body: JSON.stringify({ message: 'Hello from Lambda 1' })
 };
 }
